fd11dd9af5ca793656777bba6ffc25614def4896
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-09-16 23:48:22 +0200

    app finished

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clear = document.getElementById("clear");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let size = 20;
+let size = 10;
 let isPressed = false;
-let color = "black";
+let color;
 let x;
 let y;
 
@@ -45,3 +50,29 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size;
+}
+
+colorEl.addEventListener("change", (e) => (color = e.target.value));
+
+increaseBtn.addEventListener("click", () => {
+  size += 5;
+  if (size > 50) {
+    size = 50;
+  }
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size -= 5;
+  if (size < 5) {
+    size = 5;
+  }
+  updateSizeOnScreen();
+});
+
+clear.addEventListener("click", () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
